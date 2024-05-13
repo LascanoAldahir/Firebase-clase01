d931c0deb35f7ea94fc0d60a94db6249181bb5f7
LascanoAldahir <aldahirlascano@gmail.com>
2024-05-13 17:40:03 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC6QvyKPwRkm4jSUl2AKwSZ1g2hvSToOao",
+    authDomain: "fir-a6f92.firebaseapp.com",
+    projectId: "fir-a6f92",
+    storageBucket: "fir-a6f92.appspot.com",
+    messagingSenderId: "984227918205",
+    appId: "1:984227918205:web:346893d140f6d4c7571c14",
+    measurementId: "G-KBESSTRCY9"
+  }
 };
 
 /*
